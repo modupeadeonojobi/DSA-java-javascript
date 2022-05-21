5091164068e18f46937619548fb9388e36678e37
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-21 17:39:04 +0100

    Add linear and constant time example

--- JavaScript - DSA/bigO/findNemo.js
+++ JavaScript - DSA/bigO/findNemo.js
@@ -1,5 +1,7 @@
+// Time complexity is O(n) or linear time ie, BigO of n.  GOOD
+// By linear time or BigO of n, the operation will increase as the input increases.
+// for loops, while loops through n items
 const nemo = ['nemo'];
-
 function foundNemo(array) {
     for (let i = 0; i < nemo.length; i++) {
         if (nemo[i] === 'nemo') {
@@ -7,5 +9,14 @@ function foundNemo(array) {
         }
     }
 }
-
 foundNemo(nemo);
+
+
+// Time complexity is O(1) or constant time ie, BigO of n. EXCELLENT
+// No matter the size of the array, the operation will always be 1 ie, constant time.
+// No loop
+const names = ['John', 'Doe', 'Jane'];
+function findJohn(array) {
+    console.log(array[0]);
+}
+findJohn(names);
