20a2414108e3b8b29dd412312da6f7602070b029
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-22 19:30:03 +0100

    Add google exercise

---
+++ JavaScript - DSA/solvingProblems/sumGoogle.js
@@ -0,0 +1,38 @@
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 3, 4, 4];
+
+
+// ================================== (1) =====================================
+// Brute force approach
+function findSumOfTarget(arr1, sum) {
+    for (let i = 0; i < arr1.length - 1; i++) {
+        for (let j = 0; j < i + 1; j++) {
+            if (arr1[i] + arr1[j] === sum) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const haveSum = findSumOfTarget(arr1, 8);
+console.log(haveSum);
+
+
+
+// ================================== (2) =====================================
+// Effiecient approach of solving the challenge
+function findSumOfTarget2(arr1, sum) {
+    const mySet = new Set();
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (mySet.has(arr1[i])) {
+            return true;
+        }
+        mySet.add(sum - arr1[i])
+    }
+    return false;
+}
+
+const haveSum2 = findSumOfTarget2(arr1, 8);
+console.log(haveSum2);
