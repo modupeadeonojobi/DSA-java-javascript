f947cb8fd7e3f51cba4a2e182075f17729711b66
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-25 12:33:32 +0100

    Update

--- Java - DSA/src/com/algorithm/imodupsy/solvingproblem/CompareArrays.java
+++ Java - DSA/src/com/algorithm/imodupsy/solvingproblem/CompareArrays.java
@@ -55,6 +55,4 @@ public class CompareArrays {
         return false;
     }
 
-
-
 }
--- JavaScript - DSA/solvingProblems/compareArrays.js
+++ JavaScript - DSA/solvingProblems/compareArrays.js
@@ -84,3 +84,149 @@ var removeElement = function(nums, val) {
 };
 
 console.log(removeElement(nums, val))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<<<<<<< HEAD
+		sendPushNotificationToCourierAndCentralAdmin(pickUpRequest,
+				courierAdminUserAccount);
+
+		// Send SMS
+		sendSMSToCourierAndCentralAdmin(pickUpRequest, courierAdminUserAccount);
+
+		// Send email
+		sendEmailToCourierAndCentralAdmin(pickUpRequest, courierAdminUserAccount);
+		return pickUpRequest;
+	}
+
+	private void sendPushNotificationToCourierAndCentralAdmin(PickUpRequest pickUpRequest,
+			Optional<UserAccount> courierAdminUserAccount) {
+		String message = "Batch request with ID - " + pickUpRequest.getRequestId()
+				+ " has been sent by SORMAS";
+		if (courierAdminUserAccount.isPresent()) {
+			pushNotificationService.sendNewBatchRequestFromSORMAS(
+					courierAdminUserAccount.get().getEmail(), message);
+		}
+
+		List<UserAccount> centralAdmins = userAccountDao
+				.findAllByRoleRole(RoleConstant.CENTRAL_ADMIN);
+		centralAdmins.forEach(centralAdmin -> {
+			pushNotificationService.sendNewBatchRequestFromSORMAS(centralAdmin.getEmail(),
+					message);
+		});
+	}
+
+	private void sendSMSToCourierAndCentralAdmin(PickUpRequest pickUpRequest,
+			Optional<UserAccount> courierAdminUserAccount) {
+		String message = "Sample batch with ID - " + pickUpRequest.getBatchId()
+				+ " has been initiated at "
+				+ pickUpRequest.getOriginalOriginFacility().getState().getName()
+				+ ADMIN_INIT_NOTIFICATION_MSG;
+
+		sendSMS(courierAdminUserAccount.get().getPhoneNumber(), message);
+
+		List<UserAccount> centralAdmins = userAccountDao
+				.findAllByRoleRole(RoleConstant.CENTRAL_ADMIN);
+		centralAdmins.forEach(centralAdmin -> {
+			sendSMS(centralAdmin.getPhoneNumber(), message);
+		});
+	}
+
+	private void sendEmailToCourierAndCentralAdmin(PickUpRequest pickUpRequest,
+			Optional<UserAccount> courierAdminUserAccount) {
+		sendInitiationMailToAdmin(pickUpRequest, courierAdminUserAccount.get());
+
+		List<UserAccount> centralAdmins = userAccountDao
+				.findAllByRoleRole(RoleConstant.CENTRAL_ADMIN);
+		centralAdmins.forEach(centralAdmin -> {
+			sendInitiationMailToAdmin(pickUpRequest, centralAdmin);
+		});
+	}
+
+	private void sendInitiationMailToAdmin(PickUpRequest pickUpRequest,
+			UserAccount courierAdmin) {
+		MailContentDto contentDto = new MailContentDto();
+		contentDto.setSubject("Batch Initialization Notification");
+		contentDto.setEmail(courierAdmin.getEmail());
+		contentDto.setFirstName(courierAdmin.getFirstName());
+		contentDto.setBatchId(pickUpRequest.getBatchId());
+		contentDto
+				.setState(pickUpRequest.getOriginalOriginFacility().getState().getName());
+		contentDto.setFacilityName(pickUpRequest.getOriginFacility().getName());
+		contentDto.setDeliveryStatus("initialize");
+
+
+		courierAdmins.forEach(admin -> {
+			String message = "Batch request with ID - " + pickUpRequest.getRequestId()
+					+ " has been sent by SORMAS";
+			pushNotificationService.sendNewBatchRequestFromSORMAS(admin.getEmail(),
+					message);
+		});
+
+		// Send SMS
+		String smsMessage = "A new batch - " + pickUpRequest.getBatchId()
+				+ " has been initiated at "
+				+ pickUpRequest.getOriginalOriginFacility().getState().getName()
+				+ " on ELSRT- Plus, Kindly visit the application to assign to a pickup agent.";
+		sendSMSToCourierAndCentralAdmin(courierAdmins, smsMessage);
+
+		// Send email
+		sendInitiationMailToAdminAndCentralAdmin(pickUpRequest, courierAdmins);
+		return pickUpRequest;
+	}
+
+	private void sendSMSToCourierAndCentralAdmin(
+			List<UserAccount> courierAdminUserAccount, String smsMessage) {
+		courierAdminUserAccount.forEach(
+				courierAdmin -> sendSMS(courierAdmin.getPhoneNumber(), smsMessage));
+		List<UserAccount> centralAdmins = userAccountDao
+				.findAllByRoleRole(RoleConstant.CENTRAL_ADMIN);
+		centralAdmins.forEach(
+				centralAdmin -> sendSMS(centralAdmin.getPhoneNumber(), smsMessage));
+	}
+
+	private void sendInitiationMailToAdminAndCentralAdmin(PickUpRequest pickUpRequest,
+			List<UserAccount> courierAdmins) {
+
+		List<UserAccount> admins = userAccountDao
+				.findAllByRoleRole(RoleConstant.CENTRAL_ADMIN);
+		admins.addAll(courierAdmins);
+
+		admins.forEach(administrator -> {
+			MailContentDto contentDto = new MailContentDto();
+			contentDto.setSubject("Batch Initialization Notification");
+			contentDto.setEmail(administrator.getEmail());
+			contentDto.setFirstName(administrator.getFirstName());
+			contentDto.setBatchId(pickUpRequest.getBatchId());
+			contentDto.setFacilityName(pickUpRequest.getOriginFacility().getName());
+			contentDto.setDeliveryStatus("initialize");
+
+			emailService.sendEmail(contentDto, BATCH_INIT_TEMPLATE);
+		});
+>>>>>>> c2312f414932c815c38d0702d45d723ca63d6d5d
