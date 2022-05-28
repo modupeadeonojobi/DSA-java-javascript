25b526f24f131b9ad7fac98279a5bd57178eceb4
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-05-28 18:45:21 +0100

    Reverse a string in javascript

---
+++ JavaScript - DSA/dataStructure/Arrays/reverseString.js
@@ -0,0 +1,31 @@
+const str = 'Hello World';
+
+// ================================== (1) =====================================
+// Using for loop
+function reverse(str) {
+
+    if (!str || str.length < 2 || typeof str !==  'string') {
+        return 'This is not a string';
+    }
+
+    let newStr = [];
+    for (let i = str.length; i >= 0; i--) {
+        newStr.push(str[i])
+    }
+   
+    return newStr.join('');
+}
+
+const strReversed = reverse(str);
+console.log(strReversed);
+
+
+// ================================== (2) =====================================
+// Using built-in javaScript method
+function reverse2(str) {
+
+    return str.split('').reverse().join('');
+}
+
+const strReversed2 = reverse2(str);
+console.log(strReversed2);
