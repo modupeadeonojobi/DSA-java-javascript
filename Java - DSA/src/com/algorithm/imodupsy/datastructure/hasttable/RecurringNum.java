03d0957e86a73a704f97ed7d85af3685597c73f5
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-06-06 22:31:00 +0100

    Recurring number in java

--- Java - DSA/src/com/algorithm/imodupsy/datastructure/hasttable/RecurringNum.java
+++ Java - DSA/src/com/algorithm/imodupsy/datastructure/hasttable/RecurringNum.java
@@ -9,5 +9,22 @@ import java.util.Hashtable;
  */
 public class RecurringNum {
 
-    
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 1, 7, 3, 5, 1, 2, 4};
+
+        int recurringChar = firstRecurringNum(nums);
+        System.out.println(recurringChar);
+    }
+
+    public static int firstRecurringNum(int[] nums) {
+        HashMap<Integer, Integer> allNums = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (allNums.containsKey(nums[i]))
+                return nums[i];
+            else
+                allNums.put(nums[i], i);
+        }
+        return 0;
+    }
 }
