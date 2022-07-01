3c5fd2353db717af093000f8b1eec8c6be36a15e
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-07-01 15:55:11 +0100

    Add defanged IP

---
+++ Java - DSA/src/com/algorithm/imodupsy/avecemma/string/DefangedIPAddress.java
@@ -0,0 +1,35 @@
+package com.algorithm.imodupsy.avecemma.string;
+
+/**
+ * @author iModupsy
+ * @created 01/07/2022
+ */
+public class DefangedIPAddress {
+
+    public static void main(String[] args) {
+        String addressIP = "255.100.50.0";
+
+//        usingRegex(addressIP);
+        usingCharArray(addressIP);
+    }
+
+    private static void usingRegex(String addressIP) {
+        String defangedIP = addressIP.replace(".", "[.]");
+        System.out.println(defangedIP);
+    }
+
+    private static void usingCharArray(String addressIP) {
+        String defangedIp =  "";
+
+        for (int i = 0; i < addressIP.length(); i++) {
+            char digit = addressIP.charAt(i);
+
+            if (digit == '.') {
+                defangedIp += "[.]";
+            } else {
+                defangedIp += digit;
+            }
+        }
+        System.out.println(defangedIp);
+    }
+}
