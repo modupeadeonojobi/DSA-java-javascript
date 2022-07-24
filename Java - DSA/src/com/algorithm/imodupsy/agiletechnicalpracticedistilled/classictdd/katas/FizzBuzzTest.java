62fc77be1c11d066c79093f68376cbd8d9d24792
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-07-24 07:12:35 +0100

    Add leap check solution using TDD

--- Java - DSA/src/com/algorithm/imodupsy/agiletechnicalpracticedistilled/classictdd/katas/FizzBuzzTest.java
+++ Java - DSA/src/com/algorithm/imodupsy/agiletechnicalpracticedistilled/classictdd/katas/FizzBuzzTest.java
@@ -10,6 +10,12 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class FizzBuzzTest {
 
+    /**
+     * Write a function that takes numbers from 1 to 100 and outputs them as a string, but for
+     * multiples of 3, it returns Fizz instead of the number, and for multiples of 5, it returns
+     * Buzz. For numbers that are multiples of both 3 and 5, it returns FizzBuzz.
+     */
+
     @Test
     public void testFizzBuzz() {
         assertEquals("1", fizzBuzz(1));
---
+++ Java - DSA/src/com/algorithm/imodupsy/agiletechnicalpracticedistilled/classictdd/katas/LeapYearTest.java
@@ -0,0 +1,43 @@
+package com.algorithm.imodupsy.agiletechnicalpracticedistilled.classictdd.katas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * @author iModupsy
+ * @created 24/07/2022
+ */
+class LeapYearTest {
+
+    /**
+     * Write a function that returns true or false depending on whether its input integer is a
+     * leap year or not. A leap year is defined as one that is divisible by 4, but is not otherwise
+     * divisible by 100 unless it is also divisible by 400. For example, 2001 is a typical common
+     * year and 1996 is a typical leap year, whereas 1900 is an atypical common year and 2000
+     * is an atypical leap year.
+     */
+
+    @Test
+    public void testLeapYear() {
+        assertEquals(true, leapYear(1996));
+        assertEquals(false , leapYear(2001));
+        assertEquals(false, leapYear(1900));
+        assertEquals(true, leapYear(2000));
+    }
+
+    private boolean leapYear(int year) {
+        boolean isLeapYear = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                } else {
+                    isLeapYear = false;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        return isLeapYear;
+    }
+}
