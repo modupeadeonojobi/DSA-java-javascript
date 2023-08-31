7fc691869686b952b1e65cb2a0cede46f3318061
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2023-08-31 10:27:17 +0100

    add selection sort

---
+++ Java - DSA/src/com/algorithm/imodupsy/bigo/sort/SelectionSort.java
@@ -0,0 +1,34 @@
+package com.algorithm.imodupsy.bigo.sort;
+
+/**
+ * @author iModupsy
+ * @created 29/08/2023
+ */
+public class SelectionSort {
+
+    private static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int lowestNumIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[lowestNumIndex]) {
+                    lowestNumIndex = j;
+                }
+            }
+
+            int smallestNum = arr[lowestNumIndex];
+
+            // position 0 put arr[3] since 2 is the smallest number
+            // lowest numberIndex become the former 0
+            arr[lowestNumIndex] = arr[i];
+            arr[i] = smallestNum;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9,14,3,2,43,11,58,22};
+        selectionSort(arr);
+        System.out.println(arr);
+    }
+
+}
